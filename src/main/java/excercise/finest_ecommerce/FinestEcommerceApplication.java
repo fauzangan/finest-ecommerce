dc74142db63f0eea4941815052b32a4b914ebded
fauzangan <fauzan12356@gmail.com>
2024-07-23 11:02:35 +0700

    first commit

---
+++ src/main/java/excercise/finest_ecommerce/FinestEcommerceApplication.java
@@ -0,0 +1,13 @@
+package excercise.finest_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinestEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinestEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/excercise/finest_ecommerce/FinestEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package excercise.finest_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinestEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
